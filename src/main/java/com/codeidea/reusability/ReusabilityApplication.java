2d1063bc8c73387d3987ae727661714bec2f6f81
rjb <>
2022-07-13 16:29:53 +0900

    init

---
+++ src/main/java/com/codeidea/reusability/ReusabilityApplication.java
@@ -0,0 +1,13 @@
+package com.codeidea.reusability;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReusabilityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReusabilityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codeidea/reusability/ReusabilityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeidea.reusability;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReusabilityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
