758350a28c834812b0e5259873ec0d71351f7ec6
rjb <>
2022-07-13 17:04:59 +0900

    [gradle multi project] - common, web 분리
    기타 기본 환경 구성

---
+++ reusability-web/src/main/java/com/codeidea/config/MvcConfig.java
@@ -0,0 +1,33 @@
+package com.codeidea.config;
+
+import java.util.List;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.codeidea.bind.DataArgumentResolver;
+import com.codeidea.interceptor.ServletInterceptor;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@RequiredArgsConstructor
+public class MvcConfig implements WebMvcConfigurer {
+
+	private final DataArgumentResolver dataResolver;
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+
+		registry.addInterceptor(new ServletInterceptor())
+				.excludePathPatterns("/css/**", "/fonts/**", "/plugin/**", "/scripts/**", "/favicon.ico");
+	}
+
+	@Override
+	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+
+		resolvers.add(dataResolver);
+	}
+}
